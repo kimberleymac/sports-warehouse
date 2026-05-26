9009d0de0ddb66b3cfb9cd9d1554b51e3c7e8808
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-05-26 20:00:56 +1000

    Converted contact form from php prototype to laravel

---
+++ resources/views/contact_us.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.app')
+@section('title', 'Sports Warehouse')
+@section('content')
+
+            <section class="site-main__section brand-partnerships">
+                <h2 class="site-main__h2 brand-partnerships__h2">Contact Us</h2>
+                <div class="brand-partnerships-wrapper">
+                    <div class="brand-partnerships__h2_p">
+                        <p class="brand-partnerships__p brand-partnerships__p--blue">Receive a 15% discount on your
+                            first online order.</p>
+                    </div>
+                    <div class="brand-partnerships__imgs">
+                        <div id="newsletter-form">
+
+                            <form action="{{ route('contactus.store') }}" method="post">
+                                @csrf
+                                <div class="form-row">
+                                    <label for="firstName">First name:</label>
+                                    <input type="text" id="firstName" name="firstName" value="{{ old('firstName') }}">
+                                    @error('firstName')
+                                    <span class="error-message">{{$message}}</span>                                        
+                                    @enderror
+                                </div>
+                                <div class="form-row">
+                                    <label for="lastName">Last name:</label>
+                                    <input type="text" id="lastName" name="lastName" value="{{ old('lastName') }}" required>
+                                    @error('lastName')
+                                    <span class="error-message">{{$message}}</span>                                        
+                                    @enderror
+                                </div>
+                                <div class="form-row">
+                                    <label for="contactNumber">Contact number:</label>
+                                    <input type="text" id="contactNumber" name="contactNumber" value="{{ old('contactNumber') }}">
+                                    @error('contactNumber')
+                                    <span class="error-message">{{$message}}</span>                                        
+                                    @enderror
+                                </div>
+                                <div class="form-row">
+                                    <label for="email">Email: </label>
+                                    <input type="email" id="email" name="email" placeholder="name@email.com.au" value="{{ old('email') }}" required>
+                                    @error('email')
+                                    <span class="error-message">{{$message}}</span>                                        
+                                    @enderror
+                                </div>
+                                <div class="form-row">
+                                    <label for="question"> Question? </label>
+                                    <textarea name="question"
+                                        id="question" value="{{ old('question') }}"></textarea>
+                                    @error('question')
+                                    <span class="error-message">{{$message}}</span>                                        
+                                    @enderror
+                                </div>
+
+                                <div class="form-row">
+                                    <label>
+                                        <input class="checkbox" type="checkbox" name="termsAndConditions" value="yes" required>
+                                        Agree to terms &amp; conditions
+                                    </label>
+                                    @error('checkbox')
+                                    <span class="error-message">{{$message}}</span>                                        
+                                    @enderror
+                                </div>
+
+                                <div class="form-row">
+
+                                    <button type="submit" name="submitRegister">Submit</button>
+                                    {{-- If the form is submitted show the success message --}}
+                                        @if (session('success'))
+                                            <div class="message">{{session('success')}}</div>
+                                        @endif
+                                    
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+@endsection
