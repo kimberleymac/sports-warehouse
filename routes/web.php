4f477cbaf1f25cf39a918d95d1d295b2e5761a3f
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-05-26 20:01:14 +1000

    Added route for contact form

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/contactus', function () {
+    return view('contact_us');
+});
+
+// calls the ContactController and runs store() method and created the shortcut name 'contact.store' for easy referencing
+Route::post('/contactus', [ContactController::class, 'store'])->name('contactus.store');
