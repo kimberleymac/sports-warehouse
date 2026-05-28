a21bcf8a2655daa32b49bb6fd7f7b4c2fafe29a4
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-05-28 20:12:48 +1000

    Added routes for products and categories pages

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,27 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
+
+//Home
 
 Route::get('/', function () {
     return view('home');
 });
+
+//Products
+
+Route::get('/products', function () {
+    return view('products');
+});
+
+Route::get('/products', [ItemController::class, 'index']);
+
+Route::get('/products/{id}', [ItemController::class,'show'])->name("products.show");
+
+// Contact Us
 
 Route::get('/contactus', function () {
     return view('contact_us');
@@ -13,3 +29,9 @@ Route::get('/contactus', function () {
 
 // calls the ContactController and runs store() method and created the shortcut name 'contact.store' for easy referencing
 Route::post('/contactus', [ContactController::class, 'store'])->name('contactus.store');
+
+
+//Categories
+
+// GET /categories/3
+Route::get('/categories/{id}', [CategoryController::class,'show'])->name("categories.show");
