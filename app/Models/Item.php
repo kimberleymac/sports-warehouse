ee06aaa759eaf58762a7487a011f27ceb9e5412c
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-05-28 20:11:01 +1000

    Created item model, added fillable columns, protected casts, image finding function

---
+++ app/Models/Item.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\File;
+
+class Item extends Model
+{
+    //Tells laravel that the primary key is called itemId
+
+    protected $primaryKey = 'itemId';
+
+
+    // Allow mass assignment (multiple property values at once)
+    protected $fillable = [
+        'itemName',
+        'photo',
+        'price',
+        'salePrice',
+        'description',
+        'featured',
+        'categoryId',
+    ];
+
+    // Cast fields as specific data types
+    protected $casts = [
+        'price' => 'decimal:2',
+        'salePrice' => 'decimal:2',
+        'featured' => 'boolean',
+        'categoryId' => 'integer',
+    ];
+
+    /**
+     * Defines a dynamic image_url property (Attribute Accessor)
+     * Usage: $item->image_url
+     */
+    protected function imageUrl(): Attribute
+    {
+        return Attribute::get(function () {
+            // Check if item has an image and if the file actually exists
+            if ($this->image && File::exists(public_path('images/products/' . $this->image))) {
+                return asset('images/products/' . $this->image);
+            }
+
+            // Fallback to a placeholder image
+            return asset('images/placeholder-image.png');
+        });
+    }
+
+    //
+    /**
+     * Get category for the item.
+     *
+     * @return BelongsTo Category for item
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
