2ba31aede6b14f24a1cc1628bb150bd26b87b2cf
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-05-28 20:11:27 +1000

    Created category controller, added function to find single category

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display details of a single item
+     *
+     * @param integer $id ITEM ID
+     */
+    public function show(int $id)
+    {
+        // Find item details (404 error if not found)
+        $category = Category::findOrFail($id);
+        
+        // Find all items in the category
+        $items = Item::where("categoryId", $id)->get();
+
+        // Pass data into the view
+        return view('show_category', ["category" => $category, 'items' => $items]);
+    }
+}
