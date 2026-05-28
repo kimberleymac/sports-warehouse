ff90125c7161c5dfe35d5937bc4b2e4c7efe0ba1
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-05-28 20:13:57 +1000

    Created products page to show a single products

---
+++ resources/views/show_product.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+@section('title', 'Sports Warehouse Products')
+@section('content')
+
+            <section class="site-main__section featured-products">
+                <h2 class="site-main__h2">Products</h2>
+                <div class="featured-products-wrapper">
+
+                    <p>This shows a single product</p>
+
+                    <p style="color:red;"> {{ $item->itemName }}</p>
+
+
+                    {{-- //TODO fix up this part --}}
+                    {{-- @include(partials._item_card) --}}
+
+                    
+
+
+            </section>
+
+@endsection
