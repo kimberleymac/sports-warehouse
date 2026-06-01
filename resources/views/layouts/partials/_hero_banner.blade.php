5f2ea160c115cd0c2649e86c25bdbee30b477c09
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-06-01 18:46:55 +1000

    Updated direct links to images to asset links, Added link to button

--- resources/views/layouts/partials/_hero_banner.blade.php
+++ resources/views/layouts/partials/_hero_banner.blade.php
@@ -1,12 +1,11 @@
-            {{-- //TODO change image file paths to assets --}}
             <section class="banner wrapper__960px">
                 <!-- The Swiper banner using pagination -->
                 <div class="swiper banner__image">
                     <div class="swiper-wrapper">
-                        <div class="swiper-slide"><img src="images/banner/banner-soccer-ball.jpg" alt=""></div>
-                        <div class="swiper-slide"><img src="images/banner/football-in-grass-with-shadow.jpg" alt="">
+                        <div class="swiper-slide"><img src={{ asset ("images/banner/banner-soccer-ball.jpg") }} alt="Soccer ball"></div>
+                        <div class="swiper-slide"><img src={{ asset ("images/banner/football-in-grass-with-shadow.jpg") }} alt="Soccer ball">
                         </div>
-                        <div class="swiper-slide"><img src="images/banner/lawn-with-soccer-ball.jpg" alt=""></div>
+                        <div class="swiper-slide"><img src={{ asset ("images/banner/lawn-with-soccer-ball.jpg") }} alt="Soccer ball"></div>
                     </div>
                     <div class="swiper-pagination"></div>
                 </div>
@@ -16,8 +15,8 @@
                 <div class="banner__overlay overlay">
                     <div class="overlay__text full-centre">
                         <h2 class="overlay__title">Sports balls</h2>
-                        <p class="overlay__tag">View out brand new range of</p>
-                        <a class="overlay__button" href="#">Shop Now</a>
+                        <p class="overlay__tag">View our brand new range of</p>
+                        <a class="overlay__button" href="/categories/5">Shop Now</a>
                     </div>
                 </div>
             </section>
