40d1072cbce5421b84db9459cb34ae5814c66197
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-06-01 18:44:11 +1000

    Removed testing code and replaced with included partial

--- resources/views/show_category.blade.php
+++ resources/views/show_category.blade.php
@@ -4,19 +4,12 @@
 
 
             <section class="site-main__section featured-products">
-                <h2 class="site-main__h2">Products</h2>
+                <h2 class="site-main__h2">{{ $category->categoryName }}</h2>
                 <div class="featured-products-wrapper">
-
-                    <p>This shows a single category</p>
-
-                    <p style="color:red;"> {{ $category->categoryName }}</p>
-
-
-                    {{-- //TODO fix up this part --}}
-                    {{-- @include(partials._item_card) --}}
                     
-
-
+                    @include('layouts.partials._product_cards')
+                    
+                </div>
             </section>
 
 @endsection
