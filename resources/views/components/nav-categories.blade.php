d4053f90dbdf0f5929b41458a96bd061a96a2bbe
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-06-01 18:53:10 +1000

    Removed testing code, replaced nav list with foreach loop for component

--- resources/views/components/nav-categories.blade.php
+++ resources/views/components/nav-categories.blade.php
@@ -1,15 +1,22 @@
-<div>
-    <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
+{{-- CAN REPLACE The list WITH A FOREACH LOOP  --}}
 
-    <p style="color:red;">Category listing goes here</p>
+{{-- <ul class="product-categories__ul">
+    <li><a href="/categories/1">Shoes</a></li>
+    <li><a href="/categories/2">Helmets</a></li>
+    <li><a href="/categories/3">Pants</a></li>
+    <li><a href="/categories/4">Tops</a></li>
+    <li><a href="/categories/5">Balls</a></li>
+    <li><a href="/categories/6">Equipment</a></li>
+    <li><a href="/categories/7">Training gear</a></li>
+</ul> --}}
+        
+{{-- When you are using the route function, it does not necessarily need the specific ID for the route, you can use the whole category and it will take the ID from that --}}
 
-</div>
-
-<div>
-    <ul>
-        @foreach ($categories as $category)
-        {{-- When you are using the route function, it does not necessarily need the specific ID for the route, you can use the whole category and it will take the ID from that --}}
-        <li><a href="{{ route("categories.show", $category) }}">{{$category->categoryName}}</a></li>
-        @endforeach
-    </ul>
-</div>
+<ul class="product-categories__ul">
+     @foreach ($categories as $category)
+    
+     <li><a href="{{ route("categories.show", $category) }}">{{$category->categoryName}}</a></li>
+    
+     @endforeach 
+</ul>
+       
