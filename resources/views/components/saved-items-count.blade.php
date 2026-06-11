9dcc14538cc2f16e26aa55079417595de410fc49
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-06-11 20:52:42 +1000

    Created saved item count component to show cart items in right top corner of page

---
+++ resources/views/components/saved-items-count.blade.php
@@ -0,0 +1,11 @@
+{{-- Displays cart item count top right of page --}}
+<span class="cart_items">{{ $count }}</span>
+{{-- @if (empty($count))
+
+<span class="cart_items">0</span>
+                
+@else
+
+
+
+@endif --}}
