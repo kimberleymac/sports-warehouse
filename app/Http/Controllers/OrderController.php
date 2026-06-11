c9918ba0a1fd895fdad3174cf655b39d6af52ff9
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-06-11 10:54:42 +1000

    Created order controller with validated fields and loop to save orderid to many items

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use App\Models\Order;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class OrderController extends Controller
+{
+    /**
+     * Checkout saved items
+     *
+     * @param Request $request HTTP request object
+     */
+    public function checkout(Request $request)
+    {  
+        //Validate the user's details
+        $validated = $request->validate([
+            'firstName' => 'required|min:3',
+            'lastName' => 'required|min:3',
+            'address' => 'required',
+            'contactNumber' => 'required|digits:10',
+            'email' => 'required|email',
+            'creditCardNumber' => 'required',
+            'expiryDate' => 'required',
+            'nameOnCard' => 'required',
+            'csv' => 'required|digits:3',
+        ]);
+
+        
+        // Get the svaed event IDs from the session
+        $savedItemIds = Session::get("saved_items", []);
+
+        
+        // Loop through each item ID and create one order
+        // TODO: Implement registration
+        foreach ($savedItemIds as $id){
+            Order::create([
+                'orderDate' => now(),
+                'firstName' => $validated['firstName'],
+                'lastName' => $validated['lastName'],
+                'address' => $validated['address'],
+                'contactNumber' => $validated['contactNumber'],
+                'email' => $validated['email'],
+                'creditCardNumber' => $validated['creditCardNumber'],
+                'expiryDate' => $validated['expiryDate'],
+                'nameOnCard' => $validated['nameOnCard'],
+                'csv' => $validated['csv'],
+                'itemId' => $id,
+            ]);
+        }
+        
+        // Clear the session (forget the saved items)
+        Session::forget("saved_items");
+
+
+
+        // Redirect with a success message 
+        //OR load a "confirmation" view with checkout/ order details
+        return redirect('/')->with("success", "Checkout completed! 🛒");
+
+
+    }
+}
