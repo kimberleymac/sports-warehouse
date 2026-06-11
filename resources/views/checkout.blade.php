82e1e7322876feadc2d30d97eb6f4812e163debf
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-06-11 10:53:31 +1000

    Created checkout blade file with order form  with required fields and submit button

---
+++ resources/views/checkout.blade.php
@@ -0,0 +1,106 @@
+@extends('layouts.app')
+@section('title', 'Sports Warehouse Checkout')
+@section('content')
+    @include('layouts.partials._hero_banner')
+
+
+            <section class="site-main__section featured-products">
+                <h2 class="site-main__h2">Checkout "{{ $savedItems->count() }}" items</h2>
+                <div class="featured-products-wrapper">
+
+                    {{-- Display errors --}}
+                    @if ($errors->any())
+                    <div class="error">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    @endif
+                <div class="brand-partnerships__imgs">
+                    <div id="newsletter-form">
+                        <form action="/checkout" method="post">
+                            @csrf
+                                <div class="form-row">
+                                    <label for="firstName">First name:</label>
+                                    <input type="text" id="firstName" name="firstName" value="{{ old('firstName') }}" placeholder="John">
+                                    @error('firstName')
+                                    <span class="error-message">{{$message}}</span>                                        
+                                    @enderror
+                                </div>
+                                <div class="form-row">
+                                    <label for="lastName">Last name:</label>
+                                    <input type="text" id="lastName" name="lastName" value="{{ old('lastName') }}" placeholder="Smith" required>
+                                    @error('lastName')
+                                    <span class="error-message">{{$message}}</span>                                        
+                                    @enderror
+                                </div>
+                                <div class="form-row">
+                                    <label for="address">Address:</label>
+                                    <input type="text" id="address" name="address" value="{{ old('address') }}" required>
+                                    @error('address')
+                                    <span class="error-message">{{$message}}</span>                                        
+                                    @enderror
+                                </div>
+                                <div class="form-row">
+                                    <label for="contactNumber">Contact number:</label>
+                                    <input type="text" id="contactNumber" name="contactNumber" value="{{ old('contactNumber') }}" required>
+                                    @error('contactNumber')
+                                    <span class="error-message">{{$message}}</span>                                        
+                                    @enderror
+                                </div>
+                                <div class="form-row">
+                                    <label for="email">Email: </label>
+                                    <input type="email" id="email" name="email" placeholder="name@email.com.au" value="{{ old('email') }}" required>
+                                    @error('email')
+                                    <span class="error-message">{{$message}}</span>                                        
+                                    @enderror
+                                </div>
+                                <div class="form-row">
+                                    <label for="creditCardNumber">Credit Card Number: </label>
+                                    <input type="creditCardNumber" id="creditCardNumber" name="creditCardNumber" placeholder="" value="{{ old('creditCardNumber') }}" required>
+                                    @error('creditCardNumber')
+                                    <span class="error-message">{{$message}}</span>                                        
+                                    @enderror
+                                </div>
+                                <div class="form-row">
+                                    <label for="nameOnCard">Name on Card: </label>
+                                    <input type="nameOnCard" id="nameOnCard" name="nameOnCard" placeholder="John Smith" value="{{ old('nameOnCard') }}" required>
+                                    @error('nameOnCard')
+                                    <span class="error-message">{{$message}}</span>                                        
+                                    @enderror
+                                </div>
+                                <div class="form-row">
+                                    <label for="expiryDate">Expiry Date: </label>
+                                    <input type="expiryDate" id="expiryDate" name="expiryDate" placeholder="" value="{{ old('expiryDate') }}" required>
+                                    @error('expiryDate')
+                                    <span class="error-message">{{$message}}</span>                                        
+                                    @enderror
+                                </div>
+                                <div class="form-row">
+                                    <label for="csv">CSV: </label>
+                                    <input type="csv" id="csv" name="csv" placeholder="" value="{{ old('csv') }}" required>
+                                    @error('csv')
+                                    <span class="error-message">{{$message}}</span>                                        
+                                    @enderror
+                                </div>
+
+                                {{-- //TODO show the items in the cart --}}
+                                <p class="form-row">You have {{ $savedItems->count() }} items in your cart.</p>
+
+                                <div class="form-row">
+
+                                    <button type="submit" name="submitRegister" class="contact-button">Complete Checkout</button>
+                                    {{-- If the form is submitted show the success message --}}
+                                        @if (session('success'))
+                                            <div class="message">{{session('success')}}</div>
+                                        @endif
+                                    
+                                </div>
+                        </form>
+                    </div>
+                </div>    
+            </section>
+
+@endsection
