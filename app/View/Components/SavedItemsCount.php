c7a875cdbee900bcf47078430829388e5a6ac54d
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-06-11 20:57:56 +1000

    Scaffolded Laravel Breeze and merged with our existing templates and routes

---
+++ app/View/Components/SavedItemsCount.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class SavedItemsCount extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    // public function render(): View|Closure|string
+    // {
+    //     return view('components.saved-items-count');
+    // }
+
+
+    /**
+     * Saved item count for component
+     */
+
+    public function render(){
+        // If we ever add saved items to the database we can use
+        // $count = Item::where('saved, 1)->count();
+
+        // count items saved to the session
+        $count = count(session('saved_items', []));
+
+        return view('components.saved-items-count', ['count'=> $count]);
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -91,3 +91,45 @@ Route::post('/contactus', [ContactController::class, 'store'])->name('contactus.
 
 // GET /categories/3
 Route::get('/categories/{category:slug}', [CategoryController::class,'show'])->name("categories.show");
+
+
+
+// CODE BREEZE ADDED
+
+use App\Http\Controllers\ProfileController;
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
+// FULL BREEZE CODE
+
+// <?php
+
+// use App\Http\Controllers\ProfileController;
+// use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// require __DIR__.'/auth.php';
