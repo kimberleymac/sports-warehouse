7fd404748c89316a61816ed36c98d4812539d616
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-06-16 18:19:30 +1000

    Converted guest page form Breeze format to custom formatting already setup

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,4 +1,20 @@
-<!DOCTYPE html>
+@extends('layouts.app')
+@section('title', 'Sports Warehouse Login')
+@section('content')
+
+    <div class='full-centre'>
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            {{ $slot }}
+        </div>
+            
+    </div>
+    
+@endsection
+
+
+
+
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -15,16 +31,19 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-[#FDFEFF]">
+            
+            <div class="site-logo">
+                    <h1><a href="/"><img class="site-logo__img" src="{{ asset('images/logo/sports-warehouse-logo.svg') }}"
+                            alt="Sports Warehouse" width="600"></a></h1>
             </div>
+            
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
     </body>
-</html>
+</html> --}}
+
+
