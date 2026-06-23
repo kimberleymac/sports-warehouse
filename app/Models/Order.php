8da6da0d705089f56602522d5be4634ffe503437
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-06-23 21:54:54 +1000

    Added in orderNumber and created an autoincrementing order number function for orders table.

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -14,6 +14,7 @@ class Order extends Model
 
     // Allow mass assignment (multiple property values at once)
     protected $fillable = [
+        'orderNumber',
         'orderDate',
         'firstName',
         'lastName',
@@ -30,6 +31,51 @@ class Order extends Model
     protected $casts = [
         'orderDate' => 'datetime',
     ];
+
+    /**
+     * Generate a unique order number for each order
+     */
+    
+    // Runs automatically when the model is first loaded
+    public static function boot()
+    {
+        // run the base eloquent model setup first
+        parent::boot();
+
+        // event listener for when a new order is created
+        static::creating(function($order){
+            // if the order number is empty use the generated order number function
+            if (empty($order->orderNumber)){
+                $order->orderNumber = self::generateOrderNumber();
+            }
+        });
+    }
+
+    // generate order number - returns a string
+    public static function generateOrderNumber(): string
+    {
+        // Format INV26-000001 and resets every year    
+        $year = now()->format('Y');           // 2026
+        $prefix = 'INV' . substr($year, -2);  // INV26
+        // Get the most recent order created
+        $lastOrder = self::whereYear('created_at', $year)
+                        ->orderBy('orderId', 'desc')
+                        ->first();
+
+        $sequence = 1;
+        // if a previous order exists, take the last 6 characters, convert to an interger and add 1
+        if ($lastOrder && $lastOrder->orderNumber) {
+            // Extract the number after the hyphen, explode separates the string
+            $parts = explode('-', $lastOrder->orderNumber);
+            if (count($parts) > 1) {
+                $sequence = (int) $parts[1] + 1;
+            }
+        }
+        // concatinate all the parts together
+        return $prefix . '-' . str_pad($sequence, 6, '0', STR_PAD_LEFT);
+    }
+
+
     
     /**
      * Get items for the order.
