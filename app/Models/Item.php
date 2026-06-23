8fc000e4a41ddae3fc374f2fff82d5c42e832b5b
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-06-23 21:52:43 +1000

    Updated belongsTo to include categoryId

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -103,7 +103,7 @@ class Item extends Model
      */
     public function category(): BelongsTo
     {
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo(Category::class, 'categoryId', 'categoryId');
     }
 
 
