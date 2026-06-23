a0870e3d986a2cbe72a30858647b4c5ce2cdb434
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-06-23 21:51:58 +1000

    Added middleware for adminOnly functionality

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // This creates a blade directive where we can use @adminOnly where it will check whether the user is
+        // both logged in and also has the admin role
+        Blade::directive('adminOnly', function(){
+            // if the user is logged in && the user has admin role
+            return "<?php if(auth()->check() && auth()->user()->isAdmin()): ?>";
+        });
+
+        Blade::directive('endAdminOnly', function(){
+            return "<?php endif; ?>";
+        });
+    
     }
 }
