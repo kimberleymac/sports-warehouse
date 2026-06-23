9a9cab1f69974bb1e6baf0e41b613a761169a29c
kimberleymac <181192818+kimberleymac@users.noreply.github.com>
2026-06-23 21:56:13 +1000

    Changed formatting and styling, Added loop to calculate total price based off all the items selected in the cart and their quantites

--- resources/views/saved.blade.php
+++ resources/views/saved.blade.php
@@ -2,33 +2,61 @@
 @section('title', 'Sports Warehouse Products')
 @section('content')
 
-            <section class="site-main__section featured-products">
-                <h2 class="site-main__h2">You have {{ $items->count() }} saved items</h2>
-                <div class="featured-products-wrapper">
+    <section class="site-main__section featured-products">
+        <h2 class="site-main__h2">Shopping cart</h2>
+        <div class="">
 
-                    {{-- Show list of saved items --}}
-
-                    @if (empty($items))
-
-                    <p>You have no saved items</p>
-                    
-                    @else
-                    
-                    @include('layouts.partials._product_cards', ['items'=> $items])
-                    
-                    @endif
-
-                    
+            {{-- <h3>Order summary <span>{{ $items->count() }} items</span></h3>
 
 
-                </div>
-                
-            </section>
-            <div>
-            {{-- //TODO CTA buttons if items have been saved --}}
+            @if (empty($items))
 
-                    <a href="/checkout" class="contact-button">
-                    CHECKOUT YOUR STUFFS</a>
-                </div>
+            <p>You have no items in the cart</p>
+            
+            @else 
+            @endif
+        
+            
+            --}}
+            
+            @include('layouts.partials._product_cards_cart', ['items'=> $items])
+            
+            
+
+            
+
+            </div>
+         
+    </section>
+
+@php
+    $total = 0;
+@endphp
+
+@foreach ($items as $item)
+    @php
+        $unitPrice = $item->salePrice ?? $item->price;
+        $total += $unitPrice * $item->quantity;
+    @endphp
+@endforeach
+
+<div class="mt-6 rounded-lg border border-gray-200 bg-white p-4 shadow-sm">
+
+    <div class="flex items-center justify-between">
+        <span class="text-gray-600">Total</span>
+
+        <span class="text-lg font-bold text-gray-900">
+            ${{ $total}}
+        </span>
+    </div>
+
+</div>
+<div>
+    <a href="/checkout" class="checkout-button">Checkout</a>
+</div>
+
+
+
+
 
 @endsection
